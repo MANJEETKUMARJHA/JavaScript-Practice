2fe1f1e8754e75c276ead12c7b36a06da125e9bd
Manjeet Kumar Jha <122790579+MANJEETKUMARJHA@users.noreply.github.com>
2026-07-19 08:25:54 +0000

    code rewrite

--- 01basic/01var.js
+++ 01basic/01var.js
@@ -16,4 +16,4 @@ prefer not to use var
 because of issue in block scope and functional scope
 */
 
-console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
