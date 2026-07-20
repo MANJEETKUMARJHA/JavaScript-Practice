0ea830d709081b10f2b3d2044eea18d82fa761c7
Manjeet Kumar Jha <122790579+MANJEETKUMARJHA@users.noreply.github.com>
2026-07-20 08:56:21 +0000

    fix: enhance comments and improve code clarity in ConversionOperation.js

--- 01basic/03ConversionOpreation.js
+++ 01basic/03ConversionOpreation.js
@@ -1,27 +1,65 @@
-let score = "33abc"
+let score = "33abc" // if we want to convert string to number then it will be NaN because "33abc" is not a valid number
+// let score = null // if we want to convert null to number then it will be 0 because null is a falsy value and it will be converted to 0
+// let score = undefined // if we want to convert undefined to number then it will be NaN because undefined is a falsy value and it will be converted to NaN
+// let score = true // if we want to convert true to number then it will be 1 because true is a truthy value and it will be converted to 1
+// let score = false // if we want to convert false to number then it will be 0 because false is a falsy value and it will be converted to 0
+//let score = "Manjeet" // if we want to convert string to number then it will be NaN because "Manjeet" is not a valid number
+
 
 console.log(typeof score);
-
 console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+let valueInNumber = Number(score); // Number() is used to convert a value to a number data type it is used anydata type to number data type
+console.log(typeof valueInNumber); // typeof is used to check the data type of a variable
+console.log(valueInNumber); // This will log NaN because "33abc" is not a valid number
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// "33" => 33 because "33" is a valid number
+// "33abc" => NaN because "33abc" is not a valid number
+// true => 1 because true is a truthy value and it will be converted to 1
+// false => 0 because false is a falsy value and it will be converted to 0
+// null => 0 because null is a falsy value and it will be converted to 0
+// undefined => NaN because undefined is a falsy value and it will be converted to NaN
 
-let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
 
-// 1 => true ; 0 => fasle
-// "" => false
-// "manjeet" => true
+                                                    /*--------*/
+                    
 
-let someNumber = 99
-let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+
+
+//let isLoggedIn = 0; // if we want to convert number to boolean then it will be false because 0 is a falsy value and it will be converted to false
+//let isLoggedIn = "" // if we want to convert empty string to boolean then it will be false because empty string is a falsy value and it will be converted to false
+//let isLoggedIn = "manjeet" // if we want to convert string to boolean then it will be true because "manjeet" is a truthy value and it will be converted to 
+let isLoggedIn = 1; // if we want to convert number to boolean then it will be true because 1 is a truthy value and it will be converted to 
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn); // Boolean() is used to convert a value to a boolean data type it is used anydata type to boolean data type
+console.log(booleanIsLoggedIn); // This will log true because 1 is a truthy value and it will be converted to true
+
+// 1 => true because 1 is a truthy value and it will be converted to true
+// 0 => false because 0 is a falsy value and it will be converted to false
+// "" => false because empty string is a falsy value and it will be converted to false
+// "manjeet" => true because "manjeet" is a truthy value and it will be converted to true
+
+
+
+                                /*--------*/
+
+
+
+let someNumber = 33; // if we want to convert number to string then it will be "33" because 33 is a valid number and it will be converted to "33"
+//let someNumber = true; // if we want to convert boolean to string then it will be "true" because true is a valid boolean and it will be converted to "true"
+//let someNumber = false; // if we want to convert boolean to string then it will be "false" because false is a valid boolean and it will be converted to "false"
+//let someNumber = null; // if we want to convert null to string then it will be "null" because null is a valid value and it will be converted to "null"
+//let someNumber = undefined; // if we want to convert undefined to string then it will be "undefined" because undefined is a valid value and it will be converted to "undefined"
+
+let StringNumber = String(someNumber); // String() is used to convert a value to a string data type it is used anydata type to string data type
+console.log(typeof StringNumber); // typeof is used to check the data type of a variable
+console.log(StringNumber); // This will log "33" because 33 is a valid number and it will be converted to "33"
+
+// 33 => "33" because 33 is a valid number and it will be converted to "33"
+// true => "true" because true is a valid boolean and it will be converted to "true"
+// false => "false" because false is a valid boolean and it will be converted to "false"
+// null => "null" because null is a valid value and it will be converted to "null"
+// undefined => "undefined" because undefined is a valid value and it will be converted to "undefined"
+
