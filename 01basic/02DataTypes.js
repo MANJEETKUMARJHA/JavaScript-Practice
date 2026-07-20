c811569146eb89bddb09982a9983dce1bdab2014
Manjeet Kumar Jha <122790579+MANJEETKUMARJHA@users.noreply.github.com>
2026-07-20 07:59:54 +0000

    fix: correct comments and improve code readability in DataTypes.js; add variable declarations in varaiable.js

--- 01basic/02DataTypes.js
+++ 01basic/02DataTypes.js
@@ -1,20 +1,32 @@
-"use strict"; // tret all JS code as new version
+"use strict"; // treat all JS code as new version
 
-// alert(3 +3) //we are using node.js
+// alert(3 + 3); // we are using node.js
 
-let name = "manjeet"
-let age = 22
-let isLoggedIn = false
+console.log(3 
+    + 
+    3) // code is not readable or code is not understandable
 
+console.log("Manjeet"); // code is readable and understandable
+
+let name = "manjeet"; // a string data type
+let age = 22; // a number data type
+let isLoggedIn = false; // a boolean data type
+let state; // undefined data type, variable is declared but not initialized
+/*
+these are premitive data types in javascript
 // number => 2 to power 53
 // bigint 
-// String => " "
+// String => " "  
 // boolean => true/false
-// null => standalone value 
-// undefined => 
-// symbol => to find unique 
+// null => standalone value its means variable is declared and initialized with null value 
+// undefined => variable is declared but not initialized
+// symbol => to find unique
+*/
 
-// object
 
-console.log(typeof undefined);
-console.log(typeof null);
+// object => key-value pair
+
+console.log(typeof "manjeet"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
---
+++ 01basic/02basic/01varaiable.js
@@ -0,0 +1,22 @@
+// Declare a variable
+const accountID = 12345; // const is used to declare a variable, const can't change
+let accountEmail = "manjeet@gmail.com"; // let is used to declare a variable, let can change
+var accountPassword = "12345"; // var is used to declare a variable, but it's not recommended
+accountCity = "Kolkata";
+let accountState; // declared but not initialized
+
+// accountID = 2 // not allowed, will throw an error
+
+accountEmail = "newemail@gmail.com"; // allowed, can change
+accountPassword = "newpassword"; // allowed, can change
+accountCity = "Jaipur"; // allowed, can change
+
+
+console.log(accountID); // This will log 12345
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+// console.log(accountID); // This will throw an error because accountID is a const and cannot be reassigned
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
