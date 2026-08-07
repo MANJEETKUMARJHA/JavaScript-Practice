99270b84298ae09bb2d39dda21c8106bb60d72da
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-08-07 22:18:33 +0530

    feat: add bigint and function examples in 02DataTypes.js

--- 01basic/02DataTypes.js
+++ 01basic/02DataTypes.js
@@ -33,9 +33,35 @@ console.log(typeof undefined); // undefined
 console.log(typeof isLoggedIn); // boolean
 console.log(typeof state); // undefined
 
+// Used for very large integers.
+let bigNumber = 123456789012345678901234567890n;
+console.log(typeof bigNumber); // bigint
 
 /******************************non-primitive data types in javascript*****************/
 
 // Array 
 let myArray = [1, 2, 3, 4, 5];
 console.log(typeof myArray); // object
+
+// object
+let myObject = {
+    name: "manjeet",
+    age: 22,
+    isLoggedIn: false
+}
+console.log(typeof myObject); // object
+
+// function
+function myFunction() {
+    console.log("Hello World");
+}
+
+// typeof Operator
+console.log(typeof 100); // number
+console.log(typeof "Hello"); // string
+console.log(typeof true);   // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(typeof function(){}); // function
