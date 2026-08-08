40c7070e4037f9d265479754dff2ac5e1370c5a0
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-08-08 22:43:44 +0530

    feat: add examples and explanations for the modulus operator in 05Operators.js

--- 01basic/05Operators.js
+++ 01basic/05Operators.js
@@ -28,3 +28,18 @@ console.log(a * 3); // This will log 30 because * is used to multiply two number
 console.log(a / 3); // This will log 3.3333333333333335 because / is used to divide two numbers
 console.log(a % 3); // This will log 1 because % is used to find the remainder of a division
 console.log(a ** 3); // This will log 1000 because ** is used to find the power of a number
+
+/*********************Important ***********************/
+
+// The % operator gives the remainder, not the percentage.
+console.log(10 % 3); // This will log 1 because 10 divided by 3 is 3 with a remainder of 1
+console.log(20 % 4); // This will log 0 because 20 divided by 4 is 5 with a remainder of 0  
+
+// It's very useful for checking even/odd numbers:
+
+console.log(10 % 2); // This will log 0 because 10 is an even number
+console.log(11 % 2); // This will log 1 because 11 is an odd number
+
+let num = 10;
+
+console.log(num % 2 === 0 ); // This will log true because 10 is an even number and the remainder when divided by 2 is 0
