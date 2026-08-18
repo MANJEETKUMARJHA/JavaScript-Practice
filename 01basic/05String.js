63482a293cd7558c61e3e72ee18f67b2dbc36944
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-08-18 21:44:05 +0530

    refactor: enhance comments for string concatenation examples in 05String.js

--- 01basic/05String.js
+++ 01basic/05String.js
@@ -3,4 +3,12 @@
 const name = "Manjeet"
 const repoCount = 50
 
-console.log(name + " " + repoCount) // Concatenation
+// console.log(name + " " + repoCount) // Concatenation and concatenation methods are : +
+// console.log(name , repoCount) // Concatenation and concatenation methods are : , this is not a good way to concatenate strings and numbers
+
+// Now day we use template literals to concatenate strings and numbers that mean ` ` whare we can use ${} to concatenate strings and numbers or crate a placeholder for variables in a string
+
+console.log(`My name is ${name} and I have ${repoCount} repos`) // Template literals are the best way to concatenate strings and numbers
+// hare ` ` is called backticks and it use for template literals and ${} is called placeholder for variables in a string
+
+// 4:30
