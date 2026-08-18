08e40ad09a4eed2fa06edc2ea958f553f1f2e6e2
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-08-18 22:04:25 +0530

    feat: add string declaration and methods examples in 05String.js

--- 01basic/05String.js
+++ 01basic/05String.js
@@ -11,4 +11,13 @@ const repoCount = 50
 console.log(`My name is ${name} and I have ${repoCount} repos`) // Template literals are the best way to concatenate strings and numbers
 // hare ` ` is called backticks and it use for template literals and ${} is called placeholder for variables in a string
 
-// 4:30
+// String declaration
+const gameName = new String("manjeet") // String object
+console.log(gameName[0]); // m because string is an array of characters and we can access each character by its index
+// console.log(typeof gameName) // String object
+console.log(gameName.__proto__);
+
+console.log(gameName.length); // 7 because manjeet has 7 characters
+console.log(gameName.toUpperCase()); // MANJEET because toUpperCase() method converts all the characters of a string to uppercase
+
+// video 2: 05String.js end at 7:00 min
