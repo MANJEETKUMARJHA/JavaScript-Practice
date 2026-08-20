ffd973ce95ea3b5dfb5fb69c74462c1ac9343a1c
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-08-20 22:36:46 +0530

    refactor: remove unused string declaration and examples in 05String.js

--- 01basic/05String.js
+++ 01basic/05String.js
@@ -11,18 +11,3 @@ const repoCount = 50
 console.log(`My name is ${name} and I have ${repoCount} repos`) // Template literals are the best way to concatenate strings and numbers
 // hare ` ` is called backticks and it use for template literals and ${} is called placeholder for variables in a string
 
-// String declaration
-const gameName = new String("manjeet") // String object
-console.log(gameName[0]); // m because string is an array of characters and we can access each character by its index
-// console.log(typeof gameName) // String object
-console.log(gameName.__proto__);
-
-console.log(gameName.length); // 7 because manjeet has 7 characters
-console.log(gameName.toUpperCase()); // MANJEET because toUpperCase() method converts all the characters of a string to uppercase
-
-// String methods are used to manipulate and work with strings in JavaScript. Some common string methods include:
-// example: toUpperCase(), toLowerCase(), charAt(), indexOf(), slice(), substring(), replace(), split(), trim(), includes(), startsWith(), endsWith() etc.
-const myName = "Manjeet"
-console.log(myName.charAt(1)); // a because charAt() method returns the character at the specified index
-console.log(myName.charAt(0)); // M because charAt() method returns the character at the specified index
-console.log(myName.indexOf("M")); // 0 because indexOf() method returns the index of the first occurrence of the specified value
