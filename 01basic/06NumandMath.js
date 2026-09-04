2af622c89b544b6f70ba10fddb5f434430ceec3e
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-09-04 20:13:43 +0530

    feat: add comments explaining toFixed() and toPrecision() methods in 06NumandMath.js

--- 01basic/06NumandMath.js
+++ 01basic/06NumandMath.js
@@ -16,3 +16,10 @@ console.log(balance);
 console.log(typeof balance.toString()); // string
 console.log(balance.toString().length); // 3
 console.log(balance.toFixed(2));
+// .toFixed(x) method is used for formatting no. for specific decimal places. and x have some value between 0 to 20.
+
+const otherNumber = 23.456789;
+
+console.log(otherNumber.toPrecision(4)) 
+// .toPrecision(x) method is used for formatting no. for specific length. and x have some value between 1 to 21. 
+
