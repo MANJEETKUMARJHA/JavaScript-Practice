a90d7e837ecdf85ab6184b072fb6516f9659e837
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-09-18 22:34:25 +0530

    feat: add example for Math.SQRT1_2 and update Math.random usage in 06NumandMath.js

--- 01basic/06NumandMath.js
+++ 01basic/06NumandMath.js
@@ -97,3 +97,4 @@ console.log(Math.random()); //Returns a pseudorandom number between 0 and 1.
 //  It is essential for making dynamic, unpredictable web behavior.
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math.SQRT1_2);
