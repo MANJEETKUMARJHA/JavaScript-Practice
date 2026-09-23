4872abdcd409fe832665e094372ecab98378f689
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-09-23 19:13:59 +0530

    feat: add current date and time logging, enhance birthday output, and clarify console log descriptions in 07TimeAndDate.js

--- 01basic/07TimeAndDate.js
+++ 01basic/07TimeAndDate.js
@@ -56,8 +56,11 @@
 // it begining of january 1, 1970, UTC
 // nowdays we use temporal but it not fully function to some old browser so, we use older version
 
+const now = new Date();
+console.log('Current date and time:', now);
+
 let myDate = new Date();
-console.log(myDate.toString());
+console.log(myDate.toString()); //toString() it converts a complex object into a plain text string that humans can easily read and systems can display.
 console.log(myDate.toDateString()); //in .toDateString() it print only date and like Sat Sep 19 2026
 console.log(myDate.toJSON()); //Used by the JSON.stringify method to enable the transformation of an object's data for JavaScript Object Notation (JSON) serialization.
 // .toJSON(), it always returns a string in UTC format (Coordinated Universal Time) using the ISO 8601 standard (YYYY-MM-DDTHH:mm:ss.sssZ).
@@ -65,13 +68,14 @@ console.log(myDate.toLocaleString()); // Returns a value as a string value appro
 console.log(myDate.toTimeString()) // Returns a time as a string value.
 console.log(myDate.toLocaleTimeString('it-IT')); //Returns a time as a string value appropriate to the host environment's current locale.
 // The parentheses () are needed because they act as the "go button" or the execution trigger for a function in JavaScript.
-
-// const now = new Date();
-// console.log('Current date and time:', now);
+console.log(typeof myDate); // it is object 
 
 
-// // for specific date and time 
-// const birthday = new Date('1999-12-25T08:30:00');
-// console.log('') // This simply prints an empty line to the console. it create visual spacing terminal
-// console.log('Birthday:', birthday.toString());
+
+// for specific date and time 
+const birthday = new Date('1999-12-25T08:30:00'); //'1999-12-25T08:30:00' hare the 'T' is act like separator that devide the date from time 
+console.log('') // This simply prints an empty line to the console. it create visual spacing terminal
+console.log('Birthday:', birthday.toString());
+console.log(birthday)
+
 
