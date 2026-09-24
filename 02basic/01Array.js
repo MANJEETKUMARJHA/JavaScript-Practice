83ac2154e27b22f81351a940a36f8482db3854bd
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-09-24 20:51:52 +0530

    feat: add initial array examples and explanations in 01Array.js

--- 02basic/01Array.js
+++ 02basic/01Array.js
@@ -1,1 +1,13 @@
-// 
+// Array enable the storing a collection of mutiple items under a single variable name and has
+// members for performing common array opperation
+
+// javascript  arrays size is resizable and contain a mix of diffrent datatypes. 
+// array elements cannot be accessed using arbitray string as index i.e, array can acces like [0]
+// javascript array copy aperation craete shallow copies(share same refrence), deep copies(shere not same refrence)
+
+const myArr = [0, 1, 2, 3, 4];
+const myHeros = ["manjeet", "ironman", "batman"];
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+ 
