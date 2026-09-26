d2133a26c598e8e98cc4b4af8dc6425c10eb09b3
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-09-26 22:55:37 +0530

    feat: enhance array methods demonstration with additional examples in 01Array.js

--- 02basic/01Array.js
+++ 02basic/01Array.js
@@ -13,7 +13,23 @@ console.log(myArr[1]);
 
 
 // array methods
-myArr.push(14) // .push() it add a new elements to the end of an array, and returns the new length of the array.
-console.log(myArr)
-myArr.pop(); //.pop() Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
-console.log(myArr)
+myArr.push(14) // .push():- it add a new elements to the end of an array, and returns the new length of the array.
+console.log(myArr);
+
+myArr.pop(); //.pop():- it Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+console.log(myArr);
+
+myArr.unshift(0); //.unshift():- Inserts new elements at the start of an array, and returns the new length of the array.
+console.log(myArr);
+
+myArr.shift(); // Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+console.log(myArr);
+
+console.log(myArr.includes(7)); // .include():- Determines whether an array includes a certain element, returning true or false as appropriate.
+console.log(myArr.indexOf(4)); //.indexof():-  Returns the index of the first occurrence of a value in an array, or -1 if it is not present.
+
+const newArr = myArr.join(); //.join() Adds all the elements of an array into a string, separated by the specified separator string.
+// it beasicly convert in to string 
+
+console.log(newArr); 
+console.log(typeof newArr)
