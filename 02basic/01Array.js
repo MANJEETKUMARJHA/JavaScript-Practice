6d06db7f9b55d5209743dbb2f04c66f3654dd0c2
MANJEETKUMARJHA <manjeetjha502@gmail.com>
2026-09-26 22:46:22 +0530

    feat: update array example and add methods demonstration in 01Array.js

--- 02basic/01Array.js
+++ 02basic/01Array.js
@@ -5,9 +5,15 @@
 // array elements cannot be accessed using arbitray string as index i.e, array can acces like [0]
 // javascript array copy aperation craete shallow copies(share same refrence), deep copies(shere not same refrence)
 
-const myArr = [0, 1, 2, 3, 4];
+const myArr = [0, 5, 2, 3, 4];
 const myHeros = ["manjeet", "ironman", "batman"];
 
 const myArr2 = new Array(1, 2, 3, 4)
 console.log(myArr[1]);
- 
+
+
+// array methods
+myArr.push(14) // .push() it add a new elements to the end of an array, and returns the new length of the array.
+console.log(myArr)
+myArr.pop(); //.pop() Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+console.log(myArr)
